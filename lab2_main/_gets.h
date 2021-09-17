6fa51b5f3235a53b3fabb2f79f7d2ff8e7211927
hpceen <hpceen@mail.ru>
2021-09-17 12:59:13 +0300

    added try/catch

--- lab2_main/_gets.h
+++ lab2_main/_gets.h
@@ -6,7 +6,8 @@
 template<typename T>
 bool is_correct_get(T &a) {
     std::cin >> a;
-    if (!std::cin.good() || std::cin.eof()) {
+    if (std::cin.eof()) throw -1;
+    if (!std::cin.good()) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         return false;
@@ -16,9 +17,15 @@ bool is_correct_get(T &a) {
 
 template<typename T>
 T get_num() {
-    T num;
-    while (!is_correct_get(num)) {
-        std::cout << "Введено не число." << std::endl;
+    T num = 0;
+    try {
+        while (!is_correct_get(num)) {
+            std::cout << "Введено не число." << std::endl;
+        }
+    }
+
+    catch (int a) {
+        std::cerr << "Ошибка: " << a << std::endl;
     }
     return num;
 }
